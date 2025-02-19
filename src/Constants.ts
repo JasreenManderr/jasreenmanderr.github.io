54043caf8df9032ee7fb23e9a8f96b48b20c5c11
JasreenManderr <jasreenmanderdesigns@gmail.com>
2025-02-19 17:52:56 -0500

    Delete src directory

--- src/Constants.ts
+++
@@ -1,5 +0,0 @@
-const LINKEDIN_URL = "https://ca.linkedin.com/in/jasreen-mander-1125a825a";
-
-const ARTSTATION_URL = "https://www.artstation.com/jasreen_mander";
-
-export { LINKEDIN_URL, ARTSTATION_URL };
